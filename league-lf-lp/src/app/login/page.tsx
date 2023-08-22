65cb5fa87bec0b72f37bed33728c6fd48cc528bd
edwxrdq <edwardrquesada@gmail.com>
2023-08-21 21:25:53 -0700

    added dropdown menu to login page

--- league-lf-lp/src/app/login/page.tsx
+++ league-lf-lp/src/app/login/page.tsx
@@ -2,14 +2,31 @@ import Image from 'next/image';
 
 export default function aboutPage() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="bg-gray-300 flex min-h-screen flex-col items-center justify-between p-24">
             <div className="left-0 top-0 flex w-full justify-start font-mono text-4xl lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     LOGIN
                 </p>
             </div>
 
-            <div className="left-0 top-0 flex w-full justify-start font-mono text-4xl lg:flex">
+            <div>
+                <button class="block h-20 w-20">
+                    <img class="h-full w-full object-cover rounded-full overflow-hidden border-2 border-blue-500 focus:outline-none focus:border-white" 
+                        src="/EdwardPic.JPG"
+                        alt="Edward Picture"
+                        className="lg:rounded-xl border-b border-gray-300"
+                        width={300}
+                        height={24}
+                    />
+                </button>
+                <div class="mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
+                    <a href="#"class="block px-4 py-2 text-gray-800 hover:bg-blue-200 hover:text-white"> Account settings</a>
+                    <a href="#"class="block px-4 py-2 text-gray-800 hover:bg-blue-200 hover:text-white"> Support</a>
+                    <a href="#"class="block px-4 py-2 text-gray-800 hover:bg-blue-200 hover:text-white"> Sign out</a>
+                </div>
+            </div>
+            
+            {/* <div className="left-0 top-0 flex w-full justify-start font-mono text-4xl lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     Username:
                 </p>
@@ -19,7 +36,10 @@ export default function aboutPage() {
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     Password:
                 </p>
-            </div>
+            </div> */}
+
+        
+
 
 
 
