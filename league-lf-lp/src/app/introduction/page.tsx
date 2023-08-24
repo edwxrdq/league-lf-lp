9533072513fb743833034a055554c58454b6f8d0
edwxrdq <edwardrquesada@gmail.com>
2023-08-23 21:21:06 -0700

    text changes, added hyperlink to img/converted to button

--- league-lf-lp/src/app/introduction/page.tsx
+++ league-lf-lp/src/app/introduction/page.tsx
@@ -8,7 +8,7 @@ export default function aboutPage() {
                     OUR STORY
                 </p>
             </div>
-
+            <br></br>
             <div className="left-0 top-0 flex-full justify-left font-sans text-md lg:flex">
                 <p className="fixed left-0 top-0 justify-left text-left border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     With intentions of creating a formidable, presentable project to showcase my coding knowledge, my progress here was made possible through the support and encouragement of my co-creator/co-owner, Marjorie (ign: JaeZhou).
@@ -26,8 +26,27 @@ export default function aboutPage() {
                     GAMES WE SUPPORT
                 </p>
             </div>
-
+            <br></br>
             <div className="left-0 top-0 flex w-full justify-center font-sans text-3xl lg:flex">
+                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                    <button class="left-0 top-0 flex w-full justify-center font-sans text-3xl lg:flex">
+                        <a href="https://www.leagueoflegends.com/en-us/">
+                            <Image
+                                src="/LeagueLogo.png"
+                                alt="League Picture"
+                                className="lg:rounded-xl border-b border-gray-300"
+                                width={1200}
+                                height={529}
+                            />
+                        </a>
+                    </button>
+                </p>
+            </div>
+
+
+            {/* non button logo if deciding to scrap button link to league, maybe unnecessary in future: */}
+
+            {/* <div className="left-0 top-0 flex w-full justify-center font-sans text-3xl lg:flex">
                 <Image
                     src="/LeagueLogo.png"
                     alt="League Picture"
@@ -35,7 +54,7 @@ export default function aboutPage() {
                     width={1200}
                     height={529}
                 />
-            </div>
+            </div> */}
 
         </main>
     )
