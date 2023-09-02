b6b8466934348c6b7cfecfa59c22a956bfe5e7f8
edwxrdq <edwardrquesada@gmail.com>
2023-09-01 22:20:54 -0700

    dropdown attempt actually lol

--- league-lf-lp/src/app/playground/page.tsx
+++ league-lf-lp/src/app/playground/page.tsx
@@ -2,16 +2,57 @@ export default function playground() {
     return (
 
         // to change background color, add "ex: bg-gray-300"
-    <main className=" flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="left-0 top-0 flex w-full justify-start font-mono text-4xl lg:flex">
+    <main>
+      {/* <div className="left-0 top-0 flex w-full justify-start font-mono text-4xl lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           PLAYGROUND
         </p>
-      </div>
+      </div> */}
+
+
+      <header class="bg-gray-900">
+
+        <div class="flex items-center justify-between px-4 py-3">
+          <div>
+            <p class="h-8 font-bold text-white text-4xl">
+              <a>LFLP.GG</a> 
+            </p>
+          </div>
+
+          <div class="px-4 pt-2 pb-4">
+            <button type="button" class="block text-white focus:text-white hover:text-gray-300 focus:outline-none">
+              <svg class="h-6 w-6"></svg><b>=</b>
+            </button>
+          </div>
+        </div>
+
+        {/* <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4"> */}
+
+        <div class="px-2 pt-2 pb-4">
+          <a href="#" class="block mt-1 px-2 py-1 text-red font-semibold rounded hover:bg-gray-800">LINK 1</a>
+          <a href="#" class="block mt-1 px-2 py-1 text-red font-semibold rounded hover:bg-gray-800">LINK 2</a>
+          <a href="#" class="block mt-1 px-2 py-1 text-red font-semibold rounded hover:bg-gray-800">LINK 3</a>
+        </div>
+        
+
+      </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 {/* in order to round border: rounded-full */}
-amazing gap in between for whatever reason
-    <div class="items-center border-2 border-white focus:outline-none">
+{/* amazing gap in between for whatever reason */}
+    {/* <div class="items-center border-2 border-white focus:outline-none">
         <div class="flex justify-center text-center bg-gray-850 rounded-t">
           <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-44 rounded-tl text-center first:text-center">Summoner Name</div>
           <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-[66px] text-center first:text-center">Position</div>
@@ -21,7 +62,7 @@ amazing gap in between for whatever reason
           <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-24 text-center first:text-center">KDA</div>
           <div class="w-8"></div>
         </div>
-    </div>
+    </div> */}
 
 
 
@@ -43,50 +84,29 @@ amazing gap in between for whatever reason
       </div> */}
 
 
-
+      {/* { <script>
+        export default {
+          data() {
+            return {
+              isOpen: false,
+            }
+          }
+        }
+      </script> } */}
+      
 
 
         </main>
 
 
+
     )
 
     // https://tailwindui.com/components/application-ui/elements/dropdowns
+
+
+    // ~ 7 min, unable to figure out the function portion and running into errors with main
+      // here's logic portion, it's just commented out above in a div class:
+      // {/* <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4"> */}
+    // https://www.youtube.com/watch?v=5mFGl566wc0&list=PL7CcGwsqRpSM3w9BT_21tUU8JN2SnyckR&index=17&ab_channel=AdamWathan
 }
-
-{/* <template>
-  <Menu as="div" class="relative inline-block text-left">
-    <div>
-      <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-        Options
-        <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-      </MenuButton>
-    </div>
-
-    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-      <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div class="py-1">
-          <MenuItem v-slot="{ active }">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Account settings</a>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Support</a>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">License</a>
-          </MenuItem>
-          <form method="POST" action="#">
-            <MenuItem v-slot="{ active }">
-              <button type="submit" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm']">Sign out</button>
-            </MenuItem>
-          </form>
-        </div>
-      </MenuItems>
-    </transition>
-  </Menu>
-</template>
-
-<script setup>
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { ChevronDownIcon } from '@heroicons/vue/20/solid'
-</script> */}
