467c08c65e97647ada61af99483e7bb9c28b0d2b
edwxrdq <edwardrquesada@gmail.com>
2023-09-04 20:32:16 -0700

    adding header to remaining pages, img corrections

--- league-lf-lp/src/app/players-available/page.tsx
+++ league-lf-lp/src/app/players-available/page.tsx
@@ -1,36 +1,69 @@
-import Image from 'next/image';
+'use client'
 
-export default function navbarPage() {
-    return (
-<main className="flex min-h-screen flex-col items-center justify-between p-24">
+import * as React from 'react';
 
-    <header>
-        <button>
-            <a href="/" class="mt-20 mb-3 text-2xl font-bold hover:text-red-200">LFLP.GG</a> 
-        </button>
-    </header>
+const Playground = () => {
+  const [open, setOpen] = React.useState(false);
 
-    <div>
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <main>
+
+        <header className="bg-gray-900">
+            <div className="flex items-center justify-between px-4 py-3">
+                <div>
+                    <button>
+                        <a href="/" class="mt-20 mb-3 text-2xl font-bold font-size-2xl hover:text-red-200">LFLP.GG</a> 
+                    </button>
+                </div>
+
+                <div>
+                    <button type="button" onClick={handleOpen}>=</button>
+                </div>
+            </div>
+        </header>
+
+        <div className='bg-gray-900 flex items-center justify-end px-4'>
+            {open ? (
+            <ul className="menu">
+                <li className="menu-item ">
+                    <button>
+                        <a href="#" className="block mt-1 px-2 py-1 font-semibold rounded hover:bg-gray-800">LINK 1</a>
+                        <a href="/" className="block mt-1 px-2 py-1 font-semibold rounded hover:bg-gray-800">LINK 2</a>
+                    </button>
+                </li>
+            </ul>
+            ) : <div></div>}
+        </div>
+
+        <br></br>
+
+        <div>
             <p className="italic flex w-full justify-between text-red200 lg:text-lefttext-4xl">
-            <a>Players Available</a> 
+                <a>Players Available</a> 
             </p>
+
             <br></br>
-            <div class="items-center border-2 border-white focus:outline-none">
-                <div class="flex justify-center text-center bg-gray-850 rounded-t">
-                <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-44 rounded-tl text-center first:text-center">Summoner Name</div>
-                <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-[66px] text-center first:text-center">Position</div>
-                <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-[66px] text-center first:text-center">Tier</div>
-                <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-[82px] text-center first:text-center">Looking for</div>
-                <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-40 text-center first:text-center">WinRate</div>
-                <div class="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-24 text-center first:text-center">KDA</div>
-                <div class="w-8"></div>
+
+            <div className="items-center border-2 border-white focus:outline-none">
+                <div className="flex justify-center text-center bg-gray-850 rounded-t">
+                    <div className="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-44 rounded-tl text-center first:text-center">Summoner Name</div>
+                    <div className="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-[66px] text-center first:text-center">Position</div>
+                    <div className="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-[66px] text-center first:text-center">Tier</div>
+                    <div className="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-[82px] text-center first:text-center">Looking for</div>
+                    <div className="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-40 text-center first:text-center">WinRate</div>
+                    <div className="text-3xs font-normal text-gray-200 bg-gray-850 py-2 px-2.5 whitespace-nowrap w-24 text-center first:text-center">KDA</div>
+                    <div className="w-8"></div>
+                </div>
             </div>
         </div>
-    </div>
 
-{/* for dropdown: https://www.youtube.com/watch?v=ZT5vwF6Ooig&list=PL7CcGwsqRpSM3w9BT_21tUU8JN2SnyckR&index=16&ab_channel=AdamWathan */}
+    </main>
+  )
 
-
-</main>
-)
 }
+
+export default Playground;
